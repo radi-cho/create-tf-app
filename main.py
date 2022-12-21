6baf0e4014f2f739ca3a6c49d554151a29a86870
Radi Cho <radicho123@gmail.com>
2022-12-21 19:30:16 +0000

    Flake8 linting.

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras import Model
 
+
+# Define a model
+class MyModel(Model):
+  def __init__(self):
+    super(MyModel, self).__init__()
+    self.conv1 = Conv2D(32, 3, activation='relu')
+    self.flatten = Flatten()
+    self.dense1 = Dense(128, activation='relu')
+    self.dense2 = Dense(10)
+
+  def call(self, x):
+    x = self.conv1(x)
+    x = self.flatten(x)
+    x = self.dense1(x)
+    return self.dense2(x)
+
+
 # Load the MNIST dataset
 mnist = tf.keras.datasets.mnist
 
@@ -14,24 +31,9 @@ x_train = x_train[..., tf.newaxis].astype("float32")
 x_test = x_test[..., tf.newaxis].astype("float32")
 
 train_ds = tf.data.Dataset.from_tensor_slices(
-    (x_train, y_train)).shuffle(10000).batch(32)
+           (x_train, y_train)).shuffle(10000).batch(32)
 
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
-
-# Define a model
-class MyModel(Model):
-  def __init__(self):
-    super(MyModel, self).__init__()
-    self.conv1 = Conv2D(32, 3, activation='relu')
-    self.flatten = Flatten()
-    self.d1 = Dense(128, activation='relu')
-    self.d2 = Dense(10)
-
-  def call(self, x):
-    x = self.conv1(x)
-    x = self.flatten(x)
-    x = self.d1(x)
-    return self.d2(x)
 
 # Create an instance of the model
 model = MyModel()
@@ -47,6 +49,7 @@ train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy
 test_loss = tf.keras.metrics.Mean(name='test_loss')
 test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
+
 # Train and evaluate
 @tf.function
 def train_step(images, labels):
@@ -61,6 +64,7 @@ def train_step(images, labels):
   train_loss(loss)
   train_accuracy(labels, predictions)
 
+
 @tf.function
 def test_step(images, labels):
   # training=False is only needed if there are layers with different
@@ -70,6 +74,7 @@ def test_step(images, labels):
 
   test_loss(t_loss)
   test_accuracy(labels, predictions)
+
 
 EPOCHS = 5
 
--- tf_app.sh
+++ tf_app.sh
@@ -10,21 +10,37 @@ create_env() {
         * ) echo "Skipping tensorflow-gpu setup.";;
     esac
 
-    conda create --name tf_app python=3.9 $tf_package pylint
+    conda create --name tf_app python=3.9 $tf_package
     conda activate tf_app
+
     pip install --upgrade pip
+    pip install flake8
+}
+
+activate_env() {
+    conda activate tf_app
 }
 
 run() {
-    conda activate tf_app
+    activate_env
     python3 main.py "$@"
+}
+
+lint() {
+    activate_env
+    if [ ! -z "$@" ]; then
+        flake8 $@
+    else
+        flake8 main.py
+    fi
 }
 
 help() {
     echo
-    echo "Usage: sh ./tf_app.sh {create_env|run}" >&2
-    echo "   create_env         Create a Conda environment (named tf_app by default) and install necessary packages."
-    echo "   run                Run the main Python script in the appropriate environment."
+    echo "Usage: sh ./tf_app.sh {create_env|run|lint}" >&2
+    echo "    create_env         Create a Conda environment (named tf_app by default) and install necessary packages."
+    echo "    run                Run the main Python script in the appropriate environment."
+    echo "    lint               Run flake8 linting. Checks main.py by default, but other filenames could be provided."
     echo
 
     exit 0
@@ -48,6 +64,8 @@ case $subcmd in
         create_env;;
     run )
         run "$@";;
+    lint )
+        lint "$@";;
     * )
         printf 'Invalid action "%s"\n' "$subcmd" >&2
         exit 1;;
