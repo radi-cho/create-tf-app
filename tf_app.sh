0cee00234c9194c2899254b05921a58e1fc6e829
Radi Cho <radicho123@gmail.com>
2022-12-21 17:58:03 +0200

    Consider tensorflow-gpu during setup.

--- tf_app.sh
+++ tf_app.sh
@@ -2,24 +2,29 @@
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 
 create_env() {
-    conda create --name tf_app python=3.9
-    conda activate tf-app
-    # conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+    tf_package="tensorflow"
+    read -p "Set up GPU support packages for Tensorflow (y/[n])? " choice
 
+    case "$choice" in
+        y|Y ) tf_package="tensorflow-gpu";;
+        * ) echo "Skipping tensorflow-gpu setup.";;
+    esac
+
+    conda create --name tf_app python=3.9 $tf_package pylint
+    conda activate tf_app
     pip install --upgrade pip
-    pip install tensorflow
 }
 
 run() {
     conda activate tf_app
-    python3 main.py
+    python3 main.py "$@"
 }
 
 help() {
     echo
     echo "Usage: sh ./tf_app.sh {create_env|run}" >&2
     echo "   create_env         Create a Conda environment (named tf_app by default) and install necessary packages."
-    echo "   run                Run the main Python script in an appropriate environment."
+    echo "   run                Run the main Python script in the appropriate environment."
     echo
 
     exit 0
@@ -29,21 +34,21 @@ cmd=${0##*/}
 subcmd=
 
 if [ -z "$subcmd" ] && [ "$#" -ge 1 ]; then
-        subcmd=$1
-        shift
+    subcmd=$1
+    shift
 fi
 
 if [ -z "$subcmd" ]; then
-        help
-        exit 1
+    help
+    exit 1
 fi
 
 case $subcmd in
-        create_env)
-                create_env;;
-        run)
-                run;;
-        *)
-                printf 'Invalid action "%s"\n' "$subcmd" >&2
-                exit 1
+    create_env )
+        create_env;;
+    run )
+        run "$@";;
+    * )
+        printf 'Invalid action "%s"\n' "$subcmd" >&2
+        exit 1;;
 esac
